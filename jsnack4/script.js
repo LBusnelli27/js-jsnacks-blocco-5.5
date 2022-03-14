29ac9f2fa4a8e693efd807ad6032d1396a3cf6d9
Busna27 <lucabusnelli2001@gmail.com>
2022-03-14 12:57:01 +0100

    todo: fix jsnack4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,10 +1,23 @@
-const arrayNum = [1, 2, 3, 4, 5, 6, 7];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let currentNum = getRandomInt((arrayNum.length - 1))
+function ciao(smallerNum, biggerNum) {
+  for (let i = smallerNum; i <= biggerNum; i++) {
+    newArray.push(arrayNum[i]);
+  }
+}
+
+
+
+const arrayNum = [1, 2, 3, 4, 5, 6, 7];
+const newArray = [];
+
+
+let currentNum = getRandomInt(0, (arrayNum.length - 1));
+let smallCurrent = getRandomInt(0, (currentNum - 1));
+ciao(currentNum, smallCurrent);
 
 console.log(currentNum);
-console.log(currentNum - (1));
+console.log(smallCurrent);
+console.log(newArray);

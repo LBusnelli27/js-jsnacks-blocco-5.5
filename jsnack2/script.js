2f4a7f5c350bcc3ae0c56fa470ce2c531fecb3bb
Busna27 <lucabusnelli2001@gmail.com>
2022-03-14 11:54:27 +0100

    C - (all jsnack2 done)

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -7,10 +7,58 @@ function reverseString(str) {
   return newString;
 }
 
-
 function negativeInt(num) {
   return num = num - (num * 2);
 }
+
+const arrayNumbers = [
+  {
+    number : 32,
+    stri : 'Ciao'
+  },
+  {
+    number : 23,
+    stri : 'Come'
+  },
+  {
+    number : 12,
+    stri : 'Stai'
+  },
+  {
+    number : 332,
+    stri : 'Io'
+  },
+  {
+    number : 3,
+    stri : 'Tutto'
+  },
+  {
+    number : 67,
+    stri : 'Bene'
+  },
+  {
+    number : 87,
+    stri : 'Come'
+  },
+  {
+    number : 9,
+    stri : 'Va'
+  },
+  {
+    number : 2,
+    stri : 'La'
+  },
+  {
+    number : 435,
+    stri : 'Vita'
+  },
+]
+
+arrayNumbers.forEach(element => {
+  console.log(negativeInt(element.number));
+
+  console.log(reverseString(element.stri));
+});
 
 console.log(reverseString(prompt("Scirvi la stringa da girare")));
 console.log(negativeInt(parseInt(prompt("Scirvi un numero"))));

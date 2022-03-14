5d52953106facafcbc83bd7e217457049d14f370
Busna27 <lucabusnelli2001@gmail.com>
2022-03-14 11:46:02 +0100

    a done

---
+++ jsnack2/script.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+  var newString = "";
+
+  for (var i = str.length - 1; i >= 0; i--) { 
+    newString += str[i];
+  }
+  return newString;
+}
+
+console.log(reverseString(prompt("Scirvi la stringa da girare")));

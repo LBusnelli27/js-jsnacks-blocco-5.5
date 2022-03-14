071514fad30868d605afcd03f6fc7aa495668d1d
Busna27 <lucabusnelli2001@gmail.com>
2022-03-14 11:59:19 +0100

    refector funcitons

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,14 +1,9 @@
 function reverseString(str) {
-  var newString = "";
-
-  for (var i = str.length - 1; i >= 0; i--) { 
-    newString += str[i];
-  }
-  return newString;
+  return str.split('').reverse().join('');
 }
 
 function negativeInt(num) {
-  return num = num - (num * 2);
+  return -num;
 }
 
 const arrayNumbers = [
